5ab71250e7aceaba090663b3c71a6d74234f7bc0
Nathan Verzemnieks <njvrzm@gmail.com>
2019-11-24 13:50:47 -0800

    Filter wordlist before recursing; refine soFar copy

--- pkg/ana.go
+++ pkg/ana.go
@@ -12,21 +12,24 @@ func findAnagrams(c anagramContext) {
 	if c.top {
 		defer close(c.out)
 	}
+	words := c.words.FilterOut(func(w Word) bool {
+		return !w.IsSubset(c.remaining)
+	})
 
-	for i, w := range c.words {
-		if w.IsSubset(c.remaining) {
-			remaining := c.remaining.Minus(w)
+	for i, w := range words {
+		remaining := c.remaining.Minus(w)
 
-			soFar := make([]Word, len(c.soFar)+1)
-			copy(soFar, append(c.soFar, w))
-			if remaining.IsEmpty() {
-				c.out <- soFar
-			} else {
-				findAnagrams(anagramContext{c.words[i:], soFar, remaining, c.out, false})
-			}
+		soFar := make(WordList, len(c.soFar), len(c.soFar)+1)
+		copy(soFar, c.soFar)
+		soFar = append(soFar, w)
+		if remaining.IsEmpty() {
+			c.out <- soFar
+		} else {
+			findAnagrams(anagramContext{words[i:], soFar, remaining, c.out, false})
 		}
 	}
 }
+
 func FindAnagrams(s string, wl WordList) chan []Word {
 	out := make(chan []Word)
 	go findAnagrams(anagramContext{wl, make([]Word, 0), NewWord(s), out, true})
