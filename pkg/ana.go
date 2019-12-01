3581fb1b386c998b35e0484ecd85d6a5c33938b3
Nathan Verzemnieks <njvrzm@gmail.com>
2019-12-01 14:44:40 -0800

    WIP: working concurrent version

--- pkg/ana.go
+++ pkg/ana.go
@@ -1,8 +1,15 @@
 package wordplay
 
-func findAnagrams(words WordList, indices []int, soFar []Word, remaining LetterBag, out chan WordList, depth int) {
+import (
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func findAnagrams(words WordList, indices []int, soFar []Word, remaining LetterBag, out chan WordList, depth int, threads int, modulus int) {
 	if depth == 0 {
-		defer close(out)
+		defer wg.Done()
 	}
 	newIndices := make([]int, 0)
 	for i := range indices {
@@ -12,17 +19,23 @@ func findAnagrams(words WordList, indices []int, soFar []Word, remaining LetterB
 	}
 
 	for i, index := range newIndices {
+		if depth == 0 && (index%threads != modulus) {
+			continue
+		}
 		w := words[index]
 		soFar[depth] = w
 		if w.letterCounts == remaining {
-			out <- append(WordList{}, soFar...)
+			out <- append(WordList{}, soFar[:depth+1]...)
 		} else {
 			findAnagrams(words,
 				newIndices[i:],
 				soFar,
 				remaining.Minus(w.letterCounts),
 				out,
-				depth+1)
+				depth+1,
+				threads,
+				modulus,
+			)
 		}
 	}
 }
@@ -33,6 +46,14 @@ func FindAnagrams(s string, words WordList) chan WordList {
 	for i := 0; i < len(words); i++ {
 		indices[i] = i
 	}
-	go findAnagrams(words, indices, make(WordList, 10), LetterBagFromString(s), out, 0)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go findAnagrams(words, indices, make(WordList, 10), LetterBagFromString(s), out, 0, runtime.NumCPU(), i)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
 	return out
 }
